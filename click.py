04668127bb50924bbcf1b8db1c37f9aff17ed5e6
mone847 <mone8.7477@gmail.com>
2026-02-08 15:27:24 +0900

    Add files via upload

--- click.py
+++ click.py
@@ -10,18 +10,14 @@ def start_button_on_click(event):
 
 def canvas_on_click(event):
     """キャンバスがクリックされたときの処理を行う関数"""
-    # モグラが表示されている場合
-    if not game["hide"]:
-        # クリック位置の取得
-        rect=canvas.getBoundingClientRect()
-        click_x=event.clientX - rect.left
-        click_y=event.clientY - rect.top
-        # モグラの位置内がクリックされたか確認
-        if (game["mx"] <= click_x <= game["mx"] + WIDTH and
-            game["my"] <= click_y <= game["my"] + WIDTH):
-            # スコアを加算
-            game["score"]+=1
-            # モグラを隠す
-            game["hide"]=True
-            # 画面を更新
-            update_screen()
+    # クリック位置の取得
+    rect=canvas.getBoundingClientRect()
+    click_x=event.clientX - rect.left
+    click_y=event.clientY - rect.top
+    # モグラの位置内がクリックされたか確認
+    if (game["mx"] <= click_x <= game["mx"] + WIDTH and
+        game["my"] <= click_y <= game["my"] + WIDTH):
+        # スコアを加算
+        game["score"]+=1
+        # モグラを隠す
+        game["hide"]=True
